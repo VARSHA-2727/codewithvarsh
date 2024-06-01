77cbdb1a07006589a96836285f866504ee698176
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-06-01 13:33:23 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,7 @@
+a=58
+b=34
+print("the value of",a,"+",b,"is",a+b)
+print("the value of",a,"-",b,"is",a-b)
+print("the value of",a,"*",b,"is",a*b)
+print("the value of",a,"/",b,"is",a/b)
+print("the value of",a,"%",b,"is",a%b)

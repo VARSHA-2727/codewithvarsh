1f00501620583df41df5cf9a90bd32ec20a4c35e
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-06-19 19:29:56 +0530

    Create for.py

---
+++ for.py
@@ -0,0 +1,21 @@
+color=["red","green","blue"]
+for color in color:
+  print(color)
+  for i in color:
+    print(i)
+
+for k in range(1,8):
+  print(k+2)
+for k in range(1,12,3) :
+  print(k)
+i= int(input("enter the user number"))
+i=int(input("enter the user number"))
+while(i<=30):
+  i= int(input("enter the user number"))
+  print(i)
+  i=i+1
+print("done with the loop")
+count = 5
+while (count < 0):
+  print(count)
+  count = count - 1

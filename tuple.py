4845a6e9f648874136703124c6a1e3780c734622
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-07-06 18:24:04 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,8 @@
+tuple=(1,3,4,5,"varsha",True)
+print( "the tup values r",tuple)
+print(len(tuple))
+print(tuple[0])
+if "345" in tuple:
+    print("yes she is present inside the tuple")
+else:
+    print("no getlost")

c65be4ee34be4e8e127fa3bdbc5c517564fada77
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-10-02 20:28:50 +0530

    Create func.js

---
+++ func.js
@@ -0,0 +1,19 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+var rem_yr=90-age;
+var days=rem_yr*365;
+var weeks=rem_yr*52;
+var months=rem_yr*12;
+console.log("You have"+days+  "days", +weeks+" week, and" +months+" monthsleft.");
+
+
+
+    
+    
+    
+    
+    
+/*************Don't change the code below**********/
+}
+lifeInWeeks(12);

aea67e17843e78003792d274b679b778d165c71f
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-10-04 13:51:31 +0530

    Create lovecal.js

---
+++ lovecal.js
@@ -0,0 +1,12 @@
+var n =prompt("enter the name");
+var m=prompt("enter ur partner name");
+var c=Math.random()*100;
+c=Math.floor(c)+1;
+if(c>70)
+{alert("your score is"+c+"you love each other like romeo and juliate");}
+    if(c>30 && c<70){
+        alert("the score is"+c);
+    }
+if(c<30){
+    alert("better breakup"+c);
+}

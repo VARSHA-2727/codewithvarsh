1042b9d0d0a58a5bfe252c165bb519f1e5d0bcdd
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-10-04 13:49:22 +0530

    Create arr.js

---
+++ arr.js
@@ -0,0 +1,8 @@
+var arraylist=["varsha","disha","reva","vams"];
+guestlist=prompt("what is your name?");
+if(arraylist.includes(guestlist)){
+    alert("welcome");
+}
+else{
+    alert("next time");
+}

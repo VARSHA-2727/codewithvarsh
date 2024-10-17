77b45770fbdafad851f5b8ff6d362bffc087fc4a
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-10-17 13:52:20 +0530

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,11 @@
+public class inherit {
+    public static void main(String[] args) {
+//        inheritance inheritance1=new inheritance(4.6,7.6,9.0);
+//        inheritance  inheritance2=new  inheritance( inheritance1);
+//
+//
+//        System.out.println(inheritance1.l+" "+inheritance1.w+" "+ inheritance1.h);
+        boxWeight box3=new boxWeight(2.3,4.5,5.6,7.8);
+        System.out.println(box3.h+" "+box3.weight+" "+box3.l);
+    }
+}

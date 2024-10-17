824b765cdd6cf7bedd844f144de005f1c173f564
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-10-17 13:51:34 +0530

    Create child.java

---
+++ child.java
@@ -0,0 +1,10 @@
+public class boxWeight extends  inheritance{
+    double weight;
+    public boxWeight(){
+        this.weight=-1;
+    }
+    public boxWeight(double w,double h,double l,double weight){
+        super(l,h,w);
+        this.weight=weight;
+    }
+}

56061471945dea455473a3c2a18fc97561ccc0b0
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-10-18 21:07:28 +0530

    Create shapes.java

---
+++ shapes.java
@@ -0,0 +1,10 @@
+package polymorphism;
+
+public class main {
+    public static void main(String[] args) {
+        shapes shape=new shapes();
+        circles circle =new circles();
+        sq sqr=new sq();
+        circle.area();
+    }
+}

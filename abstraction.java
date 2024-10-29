daf1d9fecd326ea9ed1b25fe7bdf3feab11f6032
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-10-29 19:53:28 +0530

    Create abstraction.java

---
+++ abstraction.java
@@ -0,0 +1,15 @@
+package AbstractDemo;
+
+public class main extends parent {
+    @Override
+    void career(String name) {
+        System.out.println("i am gonna become a "+name);
+
+    }
+
+    @Override
+    void parter(String name,int age) {
+        System.out.println("i love "+name+"she is "+age);
+
+    }
+}

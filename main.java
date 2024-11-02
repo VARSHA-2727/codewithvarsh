a770fb07a423f08283d60eebd94fa120037ab1e7
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-11-02 20:07:19 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public class main {
+    public static void main(String[] args) {
+//        car Car=new car();
+//        Car.acc();
+//        Car.stop();
+//        Car.start();
+        nice car=new nice();
+        car.start();
+        car.startMusic();
+    }
+}

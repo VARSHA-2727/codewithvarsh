f57279bbf9f16651c2a78fd436944afbed47aa90
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-11-07 21:00:22 +0530

    Create errorMakesClever_nameAge.java

---
+++ errorMakesClever_nameAge.java
@@ -0,0 +1,14 @@
+package errorMakesClever;
+import java.util.Scanner;
+public class nameAge {
+    public static void main(String[] args) {
+        Scanner name=new Scanner(System.in);
+        String a=name.nextLine();
+        Scanner age=new Scanner(System.in);
+        int b=name.nextInt();
+        System.out.println("My name is "+a);
+        System.out.println("My age is "+b);
+
+
+    }
+}

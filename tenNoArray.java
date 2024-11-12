ad577eac7d839fc5a2068b9f3fe0a098d77d6f74
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-11-12 15:02:35 +0530

    Create tenNoArray.java

---
+++ tenNoArray.java
@@ -0,0 +1,15 @@
+package errorMakesClever;
+import java.util.Scanner;
+public class TennoArray {
+    public static void main(String[] args) {
+//        Scanner s=new Scanner(System.in);
+//        System.out.println("enter the no");
+            int[]a=new int[10];
+            for(int i=1;i<=10;i++){
+                System.out.println(i);
+
+        }
+
+
+    }
+}

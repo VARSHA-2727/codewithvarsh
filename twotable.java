4adc8c103b2faa5eae55116e2121bc1d59cfe543
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-11-12 15:03:19 +0530

    Create twotable.java

---
+++ twotable.java
@@ -0,0 +1,10 @@
+package errorMakesClever;
+import java.util.Scanner;
+public class Two_table_forLoop {
+    public static void main(String[] args) {
+        for(int i=1;i<=20;i++){
+                System.out.println(i+"x2="+i*2);
+
+            }
+        }
+    }

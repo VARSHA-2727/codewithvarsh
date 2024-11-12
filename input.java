a5e72a4c7b942109d9801546a80dde0ee747d57a
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-11-12 15:01:44 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,15 @@
+package errorMakesClever;
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = 0;
+        do {
+            System.out.println("enter the no >10");
+            a = s.nextInt();
+        }while (a >= 10) ;
+
+                System.out.println("re enter the right input");
+
+            }
+        }

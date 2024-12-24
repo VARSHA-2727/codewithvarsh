d02573b638993ce254b74b2432f8636dc44a707f
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-12-24 20:47:43 +0530

    Create pal.py

---
+++ pal.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, n):
+		for d in str(n):
+		    if(d!=0):
+		        reverse=d[::-1]
+		        reverse=n
+		        return True
+		    else:
+		        return False
+		    

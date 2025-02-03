e2ddcc3f7806faf8a97bfd04ab000dd512a1e9b2
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2025-02-03 20:42:31 +0530

    Create max1In2DArray.py

---
+++ max1In2DArray.py
@@ -0,0 +1,15 @@
+lass Solution:
+    
+    #Function to search a given number in row-column sorted matrix.
+    def searchMatrix(self, mat, x): 
+    	n=len(mat)
+    	m=len(mat[0])
+    	row,col=0,m-1
+    	while row<n and col>=0:
+    	    if mat[row][col]==x:
+    	        return True
+    	    elif mat[row][col]>x:
+    	        col-=1
+    	    else:
+    	        row=+1
+        return False
